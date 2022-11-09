91f6e53bc0729261da8fc5b13b6415cbf56bf98c
GustavoGracioM <gustavogracio@hotmail.com>
2022-11-09 01:44:50 -0300

    implementa função top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,14 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = sorted(
+        find_news(), key=lambda d: (-d["comments_count"], d["title"])
+    )
+    list_order = [n for n in news if news.index(n) < 5]
+    list_news = [(n["title"], n["url"]) for n in list_order]
+    return list_news
 
 
 # Requisito 11
